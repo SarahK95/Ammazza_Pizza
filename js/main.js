d831e782822da02f8c5149f7fd561f66b1cf51dc
SarahKamunya <sarahkamunya254@gmail.com>
2022-03-21 20:11:55 +0300

    add changes done in html and js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 
 $(document).ready(function(){
 
+ 
+
     $("#order").on('click' , function(){
 
         var pizzaType = $(".ptype").find(":selected").val();
@@ -53,17 +55,56 @@ function pizza (type,size,toppings,crust,total,orderNo){
             var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".ptype").find(":selected").text()+" - " + newPizza.type + '</th><td id="size">' + $(".psize").find(":selected").text()+" - " + newPizza.size + '</td><td id="toopings">' +$(".ptop").find(":selected").text()+ " - " + newPizza.toppings + '</td><td id="crust">'+ $(".pcrust").find(":selected").text()+" - " + newPizza.crust+ '</td><td id="total">' + newPizza.total + '</td></tr>'
             $("#pizza").append(newRow)
 
-
         })
 
         
 
 
 
-        
+        $("#check").on('click', function(){
+            $("#dev").hide()
+            $("button#yes").hide()
+            $("button#no").hide()
 
+            $("button#check").hide()
+            $("button#add").hide()
+            $("button#order").hide()
+            priceTotal= priceTotal+ total;
+
+            result.innerHTML =`The total amount is: ${priceTotal} .Thankyou.`
+
+        })
+
+        $("#yes").click(function(){
+            $("#dev").hide()
+            $("button#yes").hide()
+            $("button#no").hide()
+           
+            $("button#check").hide()
+            $("button#add").hide()
+            $("button#order").hide()
+            alert("The delivery charge is : 300")
+
+
+            var location =prompt("Please enter your location:")
+            alert(`Your Order will be delivered to: ${location}`)
+
+            var deliveryAndTotal = priceTotal + total+ 300
+            result.innerHTML =`The total amount is: ${deliveryAndTotal} .Thankyou and see you soon.`
 
             
     })
+    
+    $("#no").click(function(){
+        $("#dev").hide()
+        $("button#yes").hide()
+        $("button#no").hide()
+        alert("Please proceed and click checkout ")
+    });
+   
+
+
+
+})
 
 })

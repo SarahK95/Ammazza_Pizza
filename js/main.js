ee6756213928dc6bf2f8eb45ecbadd8b908116be
SarahKamunya <sarahkamunya254@gmail.com>
2022-03-21 17:31:40 +0300

    add jQuery to add button to add new row

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
 
 
 
-
 $(document).ready(function(){
 
     $("#order").on('click' , function(){
@@ -20,22 +19,50 @@ $(document).ready(function(){
 
         $("#type").html($(".ptype").find(":selected").text()+"-"+pizzaType); 
         $("#size").html($(".psize").find(":selected").text()+"-"+pizzaSize);  //get text as input (eg.medium)
-        $("#toopings").html($(".ptop").find(":selected").text()+"-"+pizzaTop);
+        $("#toppings").html($(".ptop").find(":selected").text()+"-"+pizzaTop);
         $("#crust").html($(".pcrust").find(":selected").text()+"-"+pizzaCrust);
         $("#total").html(total);
 
-        function pizza (type,size,toppings,crust,total,orderNo){
+      
+function pizza (type,size,toppings,crust,total,orderNo){
 
-            this.type = type;
-            this.size = size;
-            this.toppings = toppings;
-            this.crust = crust;
-            this.total = total;
-            this.orderNo = orderNo;
+    this.type = type;
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.total = total;
+    this.orderNo = orderNo;
+
+}
         
-        }
 
-        var newPizza = new Pizza(pizzaType, pizzaType, pizzaTop, pizzaCrust, total, order);
+
+        $("#add").on('click', function(){
+            var pizzaType = $(".ptype").find(":selected").val();
+            var pizzaSize = $(".psize").find(":selected").val();
+            var pizzaTop = $(".ptop").find(":selected").val();
+            var pizzaCrust = $(".pcrust").find(":selected").val();
+            var total = parseInt(pizzaSize)+ parseInt(pizzaTop) + parseInt(pizzaCrust);
+             order = order + 1;
+             priceTotal = priceTotal + total;
+
+
+            var newPizza = new pizza(pizzaType, pizzaSize, pizzaTop, pizzaCrust, total, order);
+           
+
+            var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".ptype").find(":selected").text()+" - " + newPizza.type + '</th><td id="size">' + $(".psize").find(":selected").text()+" - " + newPizza.size + '</td><td id="toopings">' +$(".ptop").find(":selected").text()+ " - " + newPizza.toppings + '</td><td id="crust">'+ $(".pcrust").find(":selected").text()+" - " + newPizza.crust+ '</td><td id="total">' + newPizza.total + '</td></tr>'
+            $("#pizza").append(newRow)
+
+
+        })
+
+        
+
+
+
+        
+
+
             
     })
 
